91f26627821666a76ef9e42d4c9e8d40a5034fcb
Jiee <7740280@qq.com>
2018-12-12 21:54:40 +0800

    完成新闻资讯样式

--- day11-项目开始/src/components/news/NewsList.vue
+++ day11-项目开始/src/components/news/NewsList.vue
@@ -1,6 +1,19 @@
 <template>
     <div>
-        新闻资讯
+        <ul class="mui-table-view">
+            <li class="mui-table-view-cell mui-media">
+                <router-link to="javascript:;">
+                    <img class="mui-media-object mui-pull-left" src="../../images/1.jpg">
+                    <div class="mui-media-body">
+                        <p class='mui-ellipsis'>能和心爱的人一起睡觉，是件幸福的事情,可是，打呼噜怎么办？</p>
+                        <p class='mui-ellipsis'>
+                            <span>发表时间:</span>
+                            <span>点击:0次</span>
+                        </p>
+                    </div>
+                </router-link>
+            </li>
+        </ul>
     </div>
 </template>
 
@@ -10,6 +23,14 @@
     }
 </script>
 
-<style scoped>
-
+<style scoped lang="scss">
+    .mui-table-view-cell {
+        p:first-child {
+            color: black;
+        }
+        .mui-ellipsis {
+            display: flex;
+            justify-content: space-between;
+        }
+    }
 </style>
