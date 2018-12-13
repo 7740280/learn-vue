812870a0d429339a72eadc335cc92b53736412bf
Jiee <7740280@qq.com>
2018-12-13 20:41:55 +0800

    改造路由使用import()函数并且完成图片分享的路由

---
+++ day11-项目开始/src/components/photos/PhotoList.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        你好
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "PhotoList"
+    }
+</script>
+
+<style scoped lang="scss">
+
+</style>
--- day11-项目开始/src/router.js
+++ day11-项目开始/src/router.js
@@ -1,10 +1,4 @@
 import VueRouter from 'vue-router'
-import HomeContainer from './components/tabbar/HomeContainer.vue'
-import MemberContainer from './components/tabbar/MemberContainer.vue'
-import ShopcarContainer from './components/tabbar/ShopcarContainer.vue'
-import SearchContainer from './components/tabbar/SearchContainer.vue'
-import NewsList from './components/news/NewsList.vue'
-import NewsInfo from './components/news/NewsInfo.vue'
 
 
 var router = new VueRouter({
@@ -15,27 +9,31 @@ var router = new VueRouter({
         },
         {
             path: '/home',
-            component: HomeContainer,
+            component: () => import('./components/tabbar/HomeContainer.vue')
         },
         {
             path: '/member',
-            component: MemberContainer
+            component: () => import( './components/tabbar/MemberContainer.vue')
         },
         {
             path: '/car',
-            component: ShopcarContainer
+            component: () => import( './components/tabbar/ShopcarContainer.vue')
         },
         {
             path: '/search',
-            component: SearchContainer
+            component: () => import( './components/tabbar/SearchContainer.vue')
         },
         {
             path: '/home/newslist',
-            component: NewsList
+            component: () => import( './components/news/NewsList.vue')
         },
         {
             path: '/home/newsinfo/:id',
-            component: NewsInfo,
+            component: () => import('./components/news/NewsInfo.vue')
+        },
+        {
+            path: '/home/photolist',
+            component: () => import('./components/photos/PhotoList.vue')
         }
     ],
     linkActiveClass: 'mui-active'
