7a37444900df9bc9af58e0ea066c380e25ea26cc
Jiee <7740280@qq.com>
2018-12-13 22:44:20 +0800

    完成图片加载

--- day11-项目开始/src/components/photos/PhotoList.vue
+++ day11-项目开始/src/components/photos/PhotoList.vue
@@ -3,24 +3,38 @@
         <div id="slider" class="mui-slider">
             <div id="sliderSegmentedControl"
                  class="mui-scroll-wrapper mui-slider-indicator mui-segmented-control mui-segmented-control-inverted">
-                <div class="mui-scroll" >
-                    <a v-for="(item,index) in cates" :key="index" :class="['mui-control-item',item.id == 0 ? 'mui-active' : '']" href="#item1mobile" data-wid="tab-top-subpage-1.html">
+                <div class="mui-scroll">
+                    <a v-for="(item,index) in cates" :key="index"
+                       :class="['mui-control-item',item.id == 0 ? 'mui-active' : '']"
+                       href="#item1mobile"
+                       data-wid="tab-top-subpage-1.html"
+                       @click="getPhotoListByCategoryId(item.id)"
+                    >
                         {{item.title}}
                     </a>
                 </div>
             </div>
 
         </div>
+
+        <!--图片列表区域-->
+        <ul>
+            <li v-for="item in list" :key="item.id">
+                <img v-lazy="item.img_url">
+            </li>
+        </ul>
     </div>
 </template>
 
 <script>
     import mui from '../../lib/mui/js/mui.min.js'
+    import {Toast} from 'mint-ui'
 
     export default {
         name: "PhotoList",
         created: function () {
             this.getAllcategory();
+            this.getPhotoListByCategoryId(0);
         },
         mounted: function () {
             //当组件 中的DOM解构被渲染好并方法哦页面中,会执行这个钩子函数
@@ -31,17 +45,27 @@
         },
         data: function () {
             return {
-                cates: [],
+                cates: [],  //所有分类
+                list: [],    //图片列表
             }
         },
         methods: {
             getAllcategory: function () {
                 this.$http.get('/getAllCategory').then((result) => {
                     if (result.data.status === 0) {
-                        result.data.data.unshift({id:0,title:'全部'});
+                        result.data.data.unshift({id: 0, title: '全部'});
                         this.cates = result.data.data;
                     } else {
                         Toast('获取分类失败');
+                    }
+                })
+            },
+            getPhotoListByCategoryId: function (CateId) {
+                this.$http.get('/getPhotoListByCategory/' + CateId).then((result) => {
+                    if (result.data.status === 0) {
+                        this.list = result.data.data.data;
+                    } else {
+                        Toast('获取图片失败');
                     }
                 })
             }
@@ -53,5 +77,11 @@
     * {
         /*屏蔽警告,chrome支持*/
         touch-action: pan-y;
+    }
+
+    img[lazy=loading] {
+        width: 80% !important;
+        height: 300px;
+        margin: auto;
     }
 </style>
--- day11-项目开始/src/main.js
+++ day11-项目开始/src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import router from './router.js'
 import app from './App.vue'
 import axios from 'axios'
-import {Header, Tabbar, TabItem, Swipe, SwipeItem,Button} from 'mint-ui'
+import {Header, Tabbar, TabItem, Swipe, SwipeItem,Button,Lazyload} from 'mint-ui'
 import Moment from 'moment'
 //导入mint-ui样式
 import 'mint-ui/lib/style.css'
@@ -20,6 +20,7 @@ Vue.component(Tabbar.name, Tabbar);
 Vue.component(TabItem.name, TabItem);
 Vue.component(Swipe.name, Swipe);
 Vue.component(SwipeItem.name, SwipeItem);
+Vue.use(Lazyload);
 
 //定义全局根URL
 axios.defaults.baseURL = 'http://vue-shop.com/api';
