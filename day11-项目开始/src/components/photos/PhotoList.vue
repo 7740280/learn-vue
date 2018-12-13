2cdc2ebf30b83071a344315e7829d341aa0f6807
Jiee <7740280@qq.com>
2018-12-13 21:47:30 +0800

    完成图片分享分类的获取

--- day11-项目开始/src/components/photos/PhotoList.vue
+++ day11-项目开始/src/components/photos/PhotoList.vue
@@ -3,24 +3,9 @@
         <div id="slider" class="mui-slider">
             <div id="sliderSegmentedControl"
                  class="mui-scroll-wrapper mui-slider-indicator mui-segmented-control mui-segmented-control-inverted">
-                <div class="mui-scroll">
-                    <a class="mui-control-item mui-active" href="#item1mobile" data-wid="tab-top-subpage-1.html">
-                        推荐
-                    </a>
-                    <a class="mui-control-item" href="#item2mobile" data-wid="tab-top-subpage-2.html">
-                        热点
-                    </a>
-                    <a class="mui-control-item" href="#item3mobile" data-wid="tab-top-subpage-3.html">
-                        北京
-                    </a>
-                    <a class="mui-control-item" href="#item4mobile" data-wid="tab-top-subpage-4.html">
-                        社会
-                    </a>
-                    <a class="mui-control-item" href="#item5mobile" data-wid="tab-top-subpage-5.html">
-                        娱乐
-                    </a>
-                    <a class="mui-control-item" href="#item5mobile" data-wid="tab-top-subpage-5.html">
-                        科技
+                <div class="mui-scroll" >
+                    <a v-for="(item,index) in cates" :key="index" :class="['mui-control-item',item.id == 0 ? 'mui-active' : '']" href="#item1mobile" data-wid="tab-top-subpage-1.html">
+                        {{item.title}}
                     </a>
                 </div>
             </div>
@@ -34,12 +19,32 @@
 
     export default {
         name: "PhotoList",
+        created: function () {
+            this.getAllcategory();
+        },
         mounted: function () {
             //当组件 中的DOM解构被渲染好并方法哦页面中,会执行这个钩子函数
             //如果要操作元素,最好在mounted里面
             mui(".mui-scroll-wrapper").scroll({
                 deceleration: 0.0005
             })
+        },
+        data: function () {
+            return {
+                cates: [],
+            }
+        },
+        methods: {
+            getAllcategory: function () {
+                this.$http.get('/getAllCategory').then((result) => {
+                    if (result.data.status === 0) {
+                        result.data.data.unshift({id:0,title:'全部'});
+                        this.cates = result.data.data;
+                    } else {
+                        Toast('获取分类失败');
+                    }
+                })
+            }
         }
     }
 </script>
