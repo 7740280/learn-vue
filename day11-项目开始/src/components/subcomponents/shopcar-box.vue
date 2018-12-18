42677cb9f6518d8e0f349d64244e8be59292c686
Jiee <7740280@qq.com>
2018-12-18 22:04:18 +0800

    绘制购物车列表

---
+++ day11-项目开始/src/components/subcomponents/shopcar-box.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="mui-numbox" data-numbox-min='1'>
+        <button class="mui-btn mui-btn-numbox-minus" type="button">-</button>
+        <input id="test" class="mui-input-numbox" type="number"
+               @change="countChanged"
+               :value="0"
+               ref="numbox"
+               readonly/>
+        <button class="mui-btn mui-btn-numbox-plus" type="button">+</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "number-box",
+        methods: {
+            countChanged: function () {
+                this.$store.commit('updateGoodsInfo', {
+                    id: this.goodid,
+                    count: this.$refs.numbox.value,
+                })
+            }
+        },
+        props: ['initcount', 'goodsid'],
+    }
+</script>
+
+<style scoped>
+
+</style>
--- day11-项目开始/src/components/tabbar/ShopcarContainer.vue
+++ day11-项目开始/src/components/tabbar/ShopcarContainer.vue
@@ -1,13 +1,59 @@
 <template>
-    <div>4</div>
+    <div class="shopcar-container">
+        <!--列表区域-->
+        <div class="mui-card">
+            <div class="mui-card-content">
+                <div class="mui-card-content-inner">
+
+                    <div class="info">
+                        <mt-switch></mt-switch>
+                        <img src="https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=479086174,865595760&fm=200&gp=0.jpg" alt="">
+                        <div>
+                            <h4>标题</h4>
+                            <span>$2199</span>
+                            <ShopcarBox></ShopcarBox>
+                            <a href="">删除</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!--结算区域-->
+        <div class="mui-card">
+            <div class="mui-card-content">
+                <div class="mui-card-content-inner">
+                    这是一个最简单的卡片视图控件；卡片视图常用来显示完整独立的一段信息，比如一篇文章的预览图、作者信息、点赞数量等
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
+    import ShopcarBox from '../subcomponents/shopcar-box.vue'
     export default {
-        name: "ShopcarContainer"
+        name: "ShopcarContainer",
+        components:{
+            ShopcarBox,
+        }
     }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+    .shopcar-container{
+        background-color: #eeeeee;
+        overflow: hidden;
+        .info{
+            display: flex;
+            justify-content: space-between;
+            img {
+                width: 60px;
+                height: 60px;
+            }
 
+            span{
+                color: red;
+            }
+        }
+    }
 </style>
