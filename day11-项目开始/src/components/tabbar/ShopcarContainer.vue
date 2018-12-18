8e2907fc09b23f7a1b2719dfada0443d9cebe04b
Jiee <7740280@qq.com>
2018-12-18 22:42:11 +0800

    完成从后台获取数据

--- day11-项目开始/src/components/tabbar/ShopcarContainer.vue
+++ day11-项目开始/src/components/tabbar/ShopcarContainer.vue
@@ -7,7 +7,8 @@
 
                     <div class="info">
                         <mt-switch></mt-switch>
-                        <img src="https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=479086174,865595760&fm=200&gp=0.jpg" alt="">
+                        <img src="https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=479086174,865595760&fm=200&gp=0.jpg"
+                             alt="">
                         <div>
                             <h4>标题</h4>
                             <span>$2199</span>
@@ -31,19 +32,45 @@
 
 <script>
     import ShopcarBox from '../subcomponents/shopcar-box.vue'
+
     export default {
         name: "ShopcarContainer",
-        components:{
+        data: function () {
+            return {
+                goodslist: [],
+            };
+        },
+        created: function () {
+            this.getGoodsList();
+        },
+        methods: {
+            getGoodsList: function () {
+                //获取到car中所有商品的id
+                var ids = [];
+                this.$store.state.car.forEach(item => ids.push(item.id));
+                //购物车没有商品,则直接返回,不需要请求接口
+                if (ids.length <= 0) {
+                    return;
+                }
+
+                this.$http.get('/goods/getShopcarList/?ids=' + ids.join()).then((result) => {
+                    if (result.data.status === 0) {
+                        this.goodsinfo = result.data.data;
+                    }
+                })
+            }
+        },
+        components: {
             ShopcarBox,
         }
     }
 </script>
 
 <style scoped lang="scss">
-    .shopcar-container{
+    .shopcar-container {
         background-color: #eeeeee;
         overflow: hidden;
-        .info{
+        .info {
             display: flex;
             justify-content: space-between;
             img {
@@ -51,7 +78,7 @@
                 height: 60px;
             }
 
-            span{
+            span {
                 color: red;
             }
         }
